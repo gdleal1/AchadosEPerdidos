8c3939746a3c71bce4ce434ef748cf544c0bdb04
leal0607 <gleal0607@gmail.com>
2025-06-23 16:43:49 -0300

    little changes

--- GUI/Frames/AnnounceFrame.py
+++ GUI/Frames/AnnounceFrame.py
@@ -3,7 +3,6 @@ from GUI.GUIComponents.SharedComponents.LogoImage import LogoImage
 from GUI.GUIComponents.SharedComponents.CategoryOptions import CategoryOptions
 from GUI.GUIComponents.SharedComponents.LocationField import LocationField
 from GUI.GUIComponents.SharedComponents.DateField import DateField
-from GUI.GUIComponents.SharedComponents.CellphoneEntry import CellphoneEntry
 from GUI.GUIComponents.AnnounceFrameComponents.FullItemDescriptionText import FullItemDescriptionText
 from GUI.GUIComponents.AnnounceFrameComponents.AnnounceButton import AnnounceButton
 from GUI.GUIComponents.SharedComponents.BackButton import BackButton
@@ -13,7 +12,7 @@ class AnnounceFrame(ck.CTkFrame):
         super().__init__(master)
 
         self.grid_columnconfigure((0,1,2,3,4), weight=1)
-        self.grid_rowconfigure((0,1,2,3,4,5,6,7), weight=1)
+        self.grid_rowconfigure((0,1,2,3,4,5,6,7,8), weight=1)
         
         # Logo
         self.logo_label = LogoImage(self)
@@ -27,41 +26,41 @@ class AnnounceFrame(ck.CTkFrame):
         self.title_frame_label = ck.CTkLabel(self, text="===================================== Anunciar Item Encontrado =====================================", font=("Arial", 25))
         self.title_frame_label.grid(row=1, columnspan=5, padx =20, pady=(10,40), sticky="ew")
 
+        # Item name entry
+        self.item_name_label = ck.CTkLabel(self, text="Nome do item encontrado:", font=("Arial", 18,"bold"))
+        self.item_name_label.grid(row=2, columnspan=5, padx=20, pady=10, sticky="ew")
+        self.item_name_entry = ck.CTkEntry(self, placeholder_text="Digite o nome do item encontrado", font=("Arial", 16))
+        self.item_name_entry.grid(row=3, columnspan=5, padx=20, pady=10, sticky="ew")
+        
         # Description textbox
         self.description_label = ck.CTkLabel(self, text="Descrição detalhada do item:", font=("Arial", 18,"bold"))
-        self.description_label.grid(row=2, columnspan=5, padx=20, pady=10, sticky="ew")
+        self.description_label.grid(row=4, columnspan=5, padx=20, pady=10, sticky="ew")
         self.description_text = FullItemDescriptionText(self)
-        self.description_text.grid(row=3,  columnspan=5, padx=20, pady=10, sticky="ew")
+        self.description_text.grid(row=5,  columnspan=5, padx=20, pady=10, sticky="ew")
 
         # Information label
         self.info_label = ck.CTkLabel(self, text="Informações adicionais:", font=("Arial", 18,"bold"))
-        self.info_label.grid(row=4, columnspan=5, padx=20, pady=10, sticky="ew")
+        self.info_label.grid(row=6, columnspan=5, padx=20, pady=10, sticky="ew")
 
         # Category options
         self.category_combobox = CategoryOptions(self)
-        self.category_combobox.grid(row=5, column=0, padx=20, pady=10, sticky="ew")
+        self.category_combobox.grid(row=7, column=0, padx=20, pady=10, sticky="ew")
 
         # Location field
         self.location_label = ck.CTkLabel(self, text="Local/Bairro onde o item foi encontrado:")
-        self.location_label.grid(row=5, column=1, padx=(20, 0),pady=10, sticky="ew")
+        self.location_label.grid(row=7, column=1, padx=(20, 0),pady=10, sticky="ew")
         self.location_entry = LocationField(self)
-        self.location_entry.grid(row=5, column=2, padx=(0, 20), pady=10,sticky="ew")
+        self.location_entry.grid(row=7, column=2, padx=(0, 20), pady=10,sticky="ew")
 
         # Date field
         self.date_label = ck.CTkLabel(self, text="Data em que o item foi encontrado:")
-        self.date_label.grid(row=5, column=3, padx=(20, 0), pady=10, sticky="ew")
+        self.date_label.grid(row=7, column=3, padx=(20, 0), pady=10, sticky="ew")
         self.date_entry = DateField(self)
-        self.date_entry.grid(row=5, column=4, padx=(0, 20), pady=10, sticky="ew")
-
-        # Cellphone entry
-        self.cellphone_label = ck.CTkLabel(self, text="Telefone para contato:")
-        self.cellphone_label.grid(row=6, column=1, padx=(20,0), pady=20, sticky="ew")
-        self.cellphone_entry = CellphoneEntry(self)
-        self.cellphone_entry.grid(row=6, column=2, padx=(0, 20), pady=20, sticky="ew")
+        self.date_entry.grid(row=7, column=4, padx=(0, 20), pady=10, sticky="ew")
 
         # Announce button
         self.announce_button = AnnounceButton(self, frame_ref=self)
-        self.announce_button.grid(row=7, column = 1, columnspan=3, padx=20, pady= 40, sticky="ew")
+        self.announce_button.grid(row=8, column = 1, columnspan=3, padx=20, pady= 40, sticky="ew")
 
 
 
